a3d789b5626316f89b3c8dbaffbd56618f5b2b82
sudobarre <fedebarrena@gmail.com>
2023-03-02 12:55:40 -0300

    add frontend domain to cors (web) config 2nd take

--- src/main/java/com/fede/blog/config/WebConfig.java
+++ src/main/java/com/fede/blog/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "https://sudobarre.github.io/blogpost-frontend")
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "https://sudobarre.github.io")
                 .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
                         "Access-Control-Request-Headers")
